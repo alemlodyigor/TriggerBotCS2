5a948907e5729d6f32100d05a4afdffa8430b650
alemlodyigor <szeregowygpim@gmail.com>
2024-03-22 12:42:41 +0100

    added main code

--- TriggerBotCS2/Program.cs
+++ TriggerBotCS2/Program.cs
@@ -1,1 +1,29 @@
-﻿
+﻿using Swed64;
+using System.Runtime.InteropServices;
+
+Swed swed = new Swed("cs2");
+IntPtr client = swed.GetModuleBase("client.dll");
+
+IntPtr forceAttack = client + 0x1730020;
+const int HOTKEY = 0xA4;
+
+while (true)
+{
+    Console.Clear();
+
+    IntPtr localPlayerPawn = swed.ReadPointer(client, 0x17371A8);
+    int entIndex = swed.ReadInt(localPlayerPawn, 0x15A4);
+
+    if (entIndex > 0)
+    {
+        swed.WriteInt(forceAttack, 65537);
+    }
+    if (entIndex < 0)
+    {
+        swed.WriteInt(forceAttack, 256);
+    }
+    Thread.Sleep(20);
+}
+
+[DllImport("user32.dll")]
+static extern short GetAsyncKeyState(int vKey);
